5f39b54f0389b99d61bab92dd67b03c31f166701
Jiaxiao Zhou <jiazho@microsoft.com>
2022-05-11 16:28:54 -0700

    refactor: implement the new generic engine (#9)
    * Using cargo workspaces + Removed buildx dependency + Cleaned up (#6)
    
    * updated repo to use cargo workspaces + removed buildx dependency from the build process + added spink build process t
    o Makefile + using nethost instead of cni + cleaned up readmes / added tars to it
    
    * adding changes from the latest PR
    
    * impl the new generic engine
    
    * removed tmp.rs
    
    * update to upstream spin

--- containerd-shim-cehostshim-v1/src/main.rs
+++ containerd-shim-cehostshim-v1/src/main.rs
@@ -112,7 +112,9 @@ pub struct WasiContext {
 }
 
 impl Instance for Wasi {
-    fn new(id: String, cfg: &InstanceConfig) -> Self {
+    type E = wasmtime::Engine;
+    fn new(id: String, cfg: Option<&InstanceConfig<Self::E>>) -> Self {
+        let cfg = cfg.unwrap();
         Wasi {
             interupt: Arc::new(RwLock::new(None)),
             exit_code: Arc::new((Mutex::new(None), Condvar::new())),
@@ -273,8 +275,9 @@ impl Instance for Wasi {
 }
 
 impl EngineGetter for Wasi {
-    fn new_engine() -> Result<wasmtime::Engine, Error> {
-        let engine = wasmtime::Engine::new(
+    type E = wasmtime::Engine;
+    fn new_engine() -> Result<Self::E, Error> {
+        let engine = Self::E::new(
             wasmtime::Config::default()
                 .interruptable(true)
                 .cranelift_opt_level(OptLevel::Speed),
@@ -318,5 +321,5 @@ fn handle_ce(
 }
 
 fn main() {
-    shim::run::<ShimCli<Wasi>>("io.containerd.cehostshim.v1", None);
+    shim::run::<ShimCli<Wasi, _>>("io.containerd.cehostshim.v1", None);
 }
